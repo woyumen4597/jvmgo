9d95449af3feb1efc083b73c83c83eca64c845a4
jrc <woyumen4597@gmail.com>
2018-01-02 12:37:36 +0800

    First Commit

---
+++ src/jvmgo/ch05/classfile/attr_markers.go
@@ -0,0 +1,10 @@
+package classfile
+
+type DeprecatedAttribute struct{ MarkerAttribute }
+type SyntheticAttribute struct{ MarkerAttribute }
+
+type MarkerAttribute struct{}
+
+func (self *MarkerAttribute) readInfo(reader *ClassReader) {
+	//read nothing
+}
